ea3373696ae7f18815f33652fcb5feb0e9cf52ad
Eno Bassey <ennyboy@gmail.com>
2018-12-10 23:28:59 +0100

    setup devise, cancancan and rollify for api project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_29_115840) do
+ActiveRecord::Schema.define(version: 2018_12_10_222226) do
 
   create_table "industries", force: :cascade do |t|
     t.string "industry_name"
@@ -25,6 +25,17 @@ ActiveRecord::Schema.define(version: 2018_10_29_115840) do
     t.string "sector_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.string "resource_type"
+    t.integer "resource_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+    t.index ["name"], name: "index_roles_on_name"
+    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
   end
 
   create_table "stock_locals", force: :cascade do |t|
@@ -69,6 +80,29 @@ ActiveRecord::Schema.define(version: 2018_10_29_115840) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["main_sector_id"], name: "index_sub_sectors_on_main_sector_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.boolean "is_active"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+    t.index ["role_id"], name: "index_users_roles_on_role_id"
+    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
+    t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
 end
