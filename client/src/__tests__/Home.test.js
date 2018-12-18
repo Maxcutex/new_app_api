308e5fee30a28d62e61103a13f9ace7ddc9ac766
maxcutex <ennyboy@gmail.com>
2018-12-18 17:22:03 +0100

    test react setup

---
+++ client/src/__tests__/Home.test.js
@@ -0,0 +1,11 @@
+import Header from "../components/common/Header";
+
+describe("Header", () => {
+  it("passing test", () => {
+    expect(true).toBeTruthy();
+  });
+
+  it("failing test", () => {
+    expect(false).toBeTruthy();
+  });
+});
