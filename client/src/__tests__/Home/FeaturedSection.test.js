a8769316d6db4f79aad722f62666a147eade2fed
maxcutex <ennyboy@gmail.com>
2018-12-19 12:08:26 +0100

    setup circle ci, coverage and code climate

--- client/src/__tests__/Home/FeaturedSection.test.js
+++ client/src/__tests__/Home/FeaturedSection.test.js
@@ -1,4 +1,3 @@
-//import ReactDOM from "react-dom";
 import React from "react";
 import { shallow } from "enzyme";
 
@@ -8,20 +7,4 @@ describe("Featured Component", () => {
   it("renders without crashing", () => {
     let component = shallow(<FeaturedSection />);
   });
-  //   it("has shows three news components", () => {
-  //     const component = shallow(<Header />);
-  //     var node = component.find("nav");
-  //     expect(node.length).toEqual(1);
-  //   });
-  //   it("has shows a big section component", () => {
-  //     const component = shallow(<Header />);
-  //     var node = component.find("nav");
-  //     expect(node.length).toEqual(1);
-  //   });
-  //   it("has shows two small section component", () => {
-  //     const component = shallow(<Header />);
-  //     var node = component.find("nav");
-
-  //     expect(node.length).toEqual(1);
-  //   });
 });
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import "../App.css";
 import "../assets/css/style.css";
 import "../assets/css/bootstrap.css";
-// import Loader from "./Loader/Loader";
 import PriceBar from "./common/PriceBar";
 import Header from "./common/Header";
 import Home from "./Home/Home";
