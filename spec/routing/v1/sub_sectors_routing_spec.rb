37b3d27e7df316b28ef2bdc18768a19505012e40
maxcutex <ennyboy@gmail.com>
2018-12-19 22:25:30 +0100

    sort sub sector routing spec2

--- spec/routing/v1/sub_sectors_routing_spec.rb
+++ spec/routing/v1/sub_sectors_routing_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe Api::V1::SubSectorsController, type: :routing do
 
     it 'routes to #show' do
       expect(
-        get: "/api/v1/main_sectors/#{main_sector.id}/sub_sectors/1"
+        get: "/api/v1/main_sectors/#{main_sector.id}/sub_sectors/#{sub_sector.id}"
       ).to route_to(
         controller: 'api/v1/sub_sectors', action: 'show',
         main_sector_id: main_sector.id.to_s, id: sub_sector.id.to_s
@@ -35,7 +35,7 @@ RSpec.describe Api::V1::SubSectorsController, type: :routing do
 
     it 'routes to #update via PUT' do
       expect(
-        put: "/api/v1/main_sectors/#{main_sector.id}/sub_sectors/1"
+        put: "/api/v1/main_sectors/#{main_sector.id}/sub_sectors/#{sub_sector.id}"
       ).to route_to(
         controller: 'api/v1/sub_sectors', action: 'update',
         main_sector_id: main_sector.id.to_s, id: sub_sector.id.to_s
@@ -44,7 +44,7 @@ RSpec.describe Api::V1::SubSectorsController, type: :routing do
 
     it 'routes to #update via PATCH' do
       expect(
-        patch: "/api/v1/main_sectors/#{main_sector.id}/sub_sectors/1"
+        patch: "/api/v1/main_sectors/#{main_sector.id}/sub_sectors/#{sub_sector.id}"
       ).to route_to(
         controller: 'api/v1/sub_sectors', action: 'update',
         main_sector_id: main_sector.id.to_s, id: sub_sector.id.to_s
@@ -53,7 +53,7 @@ RSpec.describe Api::V1::SubSectorsController, type: :routing do
 
     it 'routes to #destroy' do
       expect(
-        delete: "/api/v1/main_sectors/#{main_sector.id}/sub_sectors/1"
+        delete: "/api/v1/main_sectors/#{main_sector.id}/sub_sectors/#{sub_sector.id}"
       ).to route_to(
         controller: 'api/v1/sub_sectors', action: 'destroy',
         main_sector_id: main_sector.id.to_s, id: sub_sector.id.to_s
